16df18aef3a324019b818bb626b939d6ae6d9bb1
mixeeff <dm.mikheev@gmail.com>
2019-01-20 19:42:12 +0300

    More not null costraints

---
+++ db/migrate/20190120163539_change_column_null2.rb
@@ -0,0 +1,6 @@
+class ChangeColumnNull2 < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :answers, :question_id, false
+    change_column_null :answers, :correct, false
+  end
+end
