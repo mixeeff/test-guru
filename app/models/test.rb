82d70ffcf6ecdb75e08e5cae19f5ea70dc62ee09
mixeeff <dm.mikheev@gmail.com>
2019-01-22 23:43:28 +0300

    Indents

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,8 @@ class Test < ApplicationRecord
   belongs_to :category
   def self.find_by_category_name(category_name)
     joins('JOIN categories ON categories.id = tests.category_id')
-    .where(categories: {title: category_name})
-    .order(title: :desc)
-    .pluck(:title)
+      .where(categories: {title: category_name})
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :passed_tests
   def passed_tests_by_level(level)
     Test.joins('JOIN passed_tests ON passed_tests.test_id = tests.id')
-    .where(level: level, passed_tests: {user_id: self.id})
+        .where(level: level, passed_tests: {user_id: id})
   end
 end
