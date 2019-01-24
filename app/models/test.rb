8ce3238320756628e5ee783a8a2664aacd70ddd3
mixeeff <dm.mikheev@gmail.com>
2019-01-24 21:20:35 +0300

    Исправлены связи. Кооректная миграция для тест/автор

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
-  has_many :questions
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: "author_id", optional: true
+  has_many :questions, dependent: :destroy
   has_many :tests_users
   has_many :users, through: :tests_users
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :tests_users
+  has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
-  has_many :test_created, class_name: "Test", foreign_key: "author_id"
+  has_many :test_created, class_name: "Test", foreign_key: "author_id", dependent: :nullify
 
   def passed_tests_by_level(level)
     tests.where(level: level)
