5e08edb0981a267637b93f6d6f9611614266bd96
mixeeff <dm.mikheev@gmail.com>
2019-01-27 21:48:45 +0300

    dependent: :destroy on has_many: through

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id", optional: true
   has_many :questions, dependent: :destroy
   has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :users, through: :tests_users, dependent: :destroy
 
   def self.find_by_category_name(category_name)
     joins(:category).where(categories: {title: category_name})
