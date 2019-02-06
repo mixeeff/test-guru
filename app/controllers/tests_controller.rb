068fc34b0b26f7cf499db9ed8c78e531d4fc1056
mixeeff <dm.mikheev@gmail.com>
2019-02-07 02:34:33 +0300

    Lesson 8. Views, helpers

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,19 +1,39 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    render plain: Test.all.pluck(:title).join("\n")
+    @tests = Test.all
   end
 
-  def show
-    test = Test.find(params[:id])
-    render plain: test.title
+  def show; end
+
+  def new
+    @test = Test.new
   end
 
-  def new; end
+  def edit; end
 
   def create
-    test = Test.create!(test_params)
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to tests_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to tests_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @test.destroy
     redirect_to tests_path
   end
 
@@ -21,6 +41,10 @@ class TestsController < ApplicationController
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 
   def rescue_with_test_not_found
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header(new)
+    s = new ? "Создание нового" : "Редактирование"
+    s << " вопроса для теста \"#{@question.test.title}\""
+  end
 end
