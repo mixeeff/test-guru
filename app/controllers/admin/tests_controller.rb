ff83087d870751312c9ba0e08a0e038cfa58a6fc
mixeeff <dm.mikheev@gmail.com>
2019-03-07 02:46:40 +0300

    Lesson_13

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,7 +16,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.created_tests.new(test_params)
     if @test.save
-      redirect_to admin_tests_path, notice: t('.success')
+      redirect_to admin_tests_path, flash: { success: t('.success') }
     else
       render :new
     end
@@ -26,7 +26,7 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to admin_tests_path, notice: t('.success')
+      redirect_to admin_tests_path, flash: { success: t('.success') }
     else
       render :new
     end
@@ -34,7 +34,7 @@ class Admin::TestsController < Admin::BaseController
 
   def destroy
     @test.destroy
-    redirect_to admin_tests_path, notice: t('.deleted')
+    redirect_to admin_tests_path, flash: { success: t('.deleted') }
   end
 
   private
