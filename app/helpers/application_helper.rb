f89317671b9be412be270d5a881d541b5cb7dc98
mixeeff <dm.mikheev@gmail.com>
2019-03-09 20:53:26 +0300

    Flash message correction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
+  ALERT_CLASSES = { notice: 'info', error: 'danger', success: 'success', alert: 'danger'}
+
   def this_year
     Date.current.year
   end
 
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
+  end
+
+  def flash_message(key, message)
+    content_tag :div, message, class: "alert alert-#{ALERT_CLASSES[key.to_sym]}"
   end
 end
