98def82ba3bbc4399ffd95ad6d95354097f88c0e
mixeeff <dm.mikheev@gmail.com>
2019-03-13 00:08:51 +0300

    Semicolons

--- app/assets/javascripts/utilities/user_sign_up.js
+++ app/assets/javascripts/utilities/user_sign_up.js
@@ -1,31 +1,31 @@
 document.addEventListener('turbolinks:load', function(){
-  var conf = document.querySelector('input[id=user_password_confirmation]')
+  var conf = document.querySelector('input[id=user_password_confirmation]');
   if (conf) {
-     conf.addEventListener('input', comparePasswords)
+     conf.addEventListener('input', comparePasswords);
    }
 })
 
 function comparePasswords() {
-  pass = document.querySelector('input[id=user_password]')
-  pass_conf = document.querySelector('input[id=user_password_confirmation]')
-  icon_x = document.querySelector('.octicon-x')
-  icon_check = document.querySelector('.octicon-check')
-  group = document.querySelector('.input-group-append')
+  pass = document.querySelector('input[id=user_password]');
+  pass_conf = document.querySelector('input[id=user_password_confirmation]');
+  icon_x = document.querySelector('.octicon-x');
+  icon_check = document.querySelector('.octicon-check');
+  group = document.querySelector('.input-group-append');
 
   if (pass_conf.value == "") {
-    group.classList.add('hide')
-    return
+    group.classList.add('hide');
+    return;
   }
 
-  group.classList.remove('hide')
+  group.classList.remove('hide');
 
   if (pass_conf.value == pass.value) {
-    icon_check.classList.remove('hide')
-    icon_x.classList.add('hide')
+    icon_check.classList.remove('hide');
+    icon_x.classList.add('hide');
   }
   else {
-    icon_x.classList.remove('hide')
-    icon_check.classList.add('hide')
+    icon_x.classList.remove('hide');
+    icon_check.classList.add('hide');
   }
 
 }
