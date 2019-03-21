cbc429f7c601482e06ffd4bc1f130a0dad3063ea
mixeeff <dm.mikheev@gmail.com>
2019-03-21 21:40:03 +0300

    Gist dependencies

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers, dependent: :destroy
   has_many :test_passages
-  has_many :gists
+  has_many :gists, dependent: :destroy
   validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :nullify
-  has_many :gists
+  has_many :gists, dependent: :destroy
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
