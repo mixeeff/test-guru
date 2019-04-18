1566d8c2f71e7f91ca7067e8e57faae4d4244817
mixeeff <dm.mikheev@gmail.com>
2019-04-19 01:08:44 +0300

    badge select localization

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -6,4 +6,8 @@ module BadgesHelper
   def available_categories
     Category.all.pluck(:title, :id)
   end
+
+  def local_rule_types
+    Badge::RULE_TYPES.map{ |rule_type| [t(".#{rule_type}"), rule_type]}
+  end
 end
